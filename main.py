52003eb942c492896f5cf4cd3fb570f2d774bcfe
Minsoo Kim <minsooerickim@gmail.com>
2022-11-20 23:58:48 -0800

    add backwards elim

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def nearest_neighbor_classifier(data, object_to_classify, current_set):
     return nearest_neighbor_label
 
 
-def leave_one_out_cross_validation(data, current_set, feature_to_add):
+def leave_one_out_cross_validation(data, current_set):
     """
     Cross Validation. Leave an object out of the rest of the set. Use the rest of the set
     to build a model. Then, use the "one out" with the model built with the other data and determine its accuracy.
@@ -37,14 +37,15 @@ def leave_one_out_cross_validation(data, current_set, feature_to_add):
     return accuracy
 
 def forward_selection(data):
+    """
+    Starts with no features then adds them one by one and returns the best set of features.
+    """
     num_features = len(data[0]) - 1
     current_set_of_features, best_features = [], []
 
     best_so_far_accuracy = 0
     for i in range(num_features):
         print(f'\nOn the {i+1} level of the search tree')
-        
-        feature_to_add_at_this_level = -1
 
         # flag to tell a better accuracy was found at each level
         better_accuracy_found = False
@@ -53,7 +54,7 @@ def forward_selection(data):
             if j not in current_set_of_features:
                 print(f'-- Considering adding the {j} feature')
                 
-                accuracy = leave_one_out_cross_validation(data, current_set_of_features+[j], j)
+                accuracy = leave_one_out_cross_validation(data, current_set_of_features+[j])
                 print(f'---- accuracy with {current_set_of_features+[j]} : {accuracy}')
 
                 if accuracy > best_so_far_accuracy:
@@ -75,6 +76,51 @@ def forward_selection(data):
 
     print(f'\nThe best features are {best_features} with accuracy {best_so_far_accuracy}')
 
+def backward_elimination(data):
+    """
+    Starts with all the features then eliminates them until the best set of features is found.
+    """
+    num_features = len(data[0]) - 1
+    current_set_of_features, best_features = [i+1 for i in range(num_features)], [i+1 for i in range(num_features)]
+
+    best_so_far_accuracy = leave_one_out_cross_validation(data, current_set_of_features)
+    for i in range(num_features):
+        print(f'\nOn the {i+1} level of the search tree')
+
+        # flag to tell a better accuracy was found at each level
+        better_accuracy_found = False
+        best_lvl_accuracy = 0
+
+        for j in range(1, num_features+1):
+            if j in current_set_of_features:
+                print(f'-- Considering removing the {j} feature')
+            
+                copy_set = [feature for feature in current_set_of_features if feature != j]
+                accuracy = leave_one_out_cross_validation(data, copy_set)
+                print(f'---- accuracy with {copy_set} : {accuracy}')
+
+                if accuracy > best_so_far_accuracy:
+                    best_so_far_accuracy = accuracy
+                    feature_to_remove_at_this_level = j
+                    better_accuracy_found = True
+                elif accuracy > best_lvl_accuracy:
+                    best_lvl_accuracy = accuracy
+                    feature_to_remove_at_this_level = j
+
+        if better_accuracy_found: 
+            best_features.remove(feature_to_remove_at_this_level)
+            current_set_of_features.remove(feature_to_remove_at_this_level)
+            print(f'\nfeature subset {best_features} had the highest accuracy of {best_so_far_accuracy}')
+        else:
+            # add feature to current_set_of_features regardless of better_accuracy_found for potential higher accuracy late on
+            current_set_of_features.remove(feature_to_remove_at_this_level)
+            print(f'\nThe best accuracy at this level, {best_lvl_accuracy} was less than the best so far accuracy of {best_so_far_accuracy}')
+
+        print(f'On level {i+1}, I removed feature {feature_to_remove_at_this_level} to current set')
+        print('-----------------------------------------------')
+
+    print(f'\nThe best features are {best_features} with accuracy {best_so_far_accuracy}')
+
 def read_data(file):
     f = open(file, 'r')
     lines = f.readlines()
@@ -90,8 +136,10 @@ def read_data(file):
     
 def main():
     data = read_data('CS170_Small_Data__88.txt')
-    forward_selection(data)
     # data = read_data('CS170_Large_Data__78.txt')
+    
+    # forward_selection(data)
+    backward_elimination(data)
 
 
 main()
