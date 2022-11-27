7b8b28d09ea41bdc8a2a03b55fc5cb42a6ffbd49
Minsoo Kim <minsooerickim@gmail.com>
2022-11-26 22:25:03 -0800

    update utility fn

--- utility.py
+++ utility.py
@@ -1,12 +1,12 @@
 # for plotting bar graphs
-import pandas as pd
+import pandas
 import matplotlib.pyplot as plot
 
 def graph(accuracy, feature_set, title):
     # A python dictionary
     data = { "Current Feature Set":feature_set, "Accuracy": accuracy }
     # load data into dataframe
-    dataFrame = pd.DataFrame(data=data)
+    dataFrame = pandas.DataFrame(data=data)
     # plot bar chart
     dataFrame.plot.bar(x='Current Feature Set', y='Accuracy', title=title)
     plot.show(block=True)
